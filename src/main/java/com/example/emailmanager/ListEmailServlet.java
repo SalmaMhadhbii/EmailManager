725a320383508d3daebed9c96707ad9d63cd5606
SalmaMhadhbii <salmamhadhbi1120@gmail.com>
2024-11-23 21:05:32 +0100

    Initial commit

---
+++ src/main/java/com/example/emailmanager/ListEmailServlet.java
@@ -0,0 +1,27 @@
+package com.example.emailmanager;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "ListEmailServlet", value = "/list-email-servlet")
+public class ListEmailServlet extends HttpServlet {
+    //list des address:
+    private List<String> emailList = new ArrayList<>();
+
+    public void init() throws ServletException {
+        // Récupérer le chemin du fichier depuis les paramètres d'initialisation
+        String filePath = getServletContext().getInitParameter("filepath");
+
+        if(filePath == null) {
+            throw new ServletException("filepath not set in web.xml");
+        }
+
+    }
+
+
+}
